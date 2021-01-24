6b8c8e2c5db78cb189edf72d98d3931d5bc6e310
Ryan Rotman <ryan.rotman@gmail.com>
2021-01-23 23:01:20 -0600

    edited return to include router switch route options

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import './App.css';
 import backgroundImg from "./assets/images/grey-wood.jpg"
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import Contact from "./pages/Contact";
 
 function App() {
   return (
-    <div style={{ backgroundImage: `url(${backgroundImg})`, backgroundRepeat: "no-repeat", height: "100%", backgroundPosition: "center", backgroundSize: "cover", backgroundAttachment: "fixed" }}>
+    // <div style={{ backgroundImage: `url(${backgroundImg})`, backgroundRepeat: "no-repeat", height: "100%", backgroundSize: "cover", backgroundAttachment: "fixed", backgroundPosition: "center" }}>
     
-    </div>
+    // </div>
+
+    <BrowserRouter>
+        <Navbar />
+        <Switch>
+          <Route exact path="/">
+            <Home/>
+          </Route>
+          <Route exact path="/contact">
+            <Contact />
+          </Route>
+        </Switch>
+    </BrowserRouter>
   );
 }
 
