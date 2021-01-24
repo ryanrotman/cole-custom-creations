e7576a7395fee23a6fa71528b4ede944d4a748ed
Ryan Rotman <ryan.rotman@gmail.com>
2021-01-23 23:02:56 -0600

    added navbar component and styling

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,36 @@
+import { useEffect } from "react";
+import { Link } from "react-router-dom";
+import M from "materialize-css";
+import "./index.css";
+
+function Navbar() {
+
+    useEffect(() => {
+        let sidenav = document.querySelector('#nav-mobile');
+        M.Sidenav.init(sidenav, {});
+    }, []);
+
+    return (
+        <div>
+            <div className="navbar-fixed">
+                <nav className="white" role="navigation">
+                    <div className="nav-wrapper container">
+                    <a id="logo-container" className="brand-logo black-text">Cole Custom Creations</a>
+                    <a href="#" data-target="nav-mobile" className="sidenav-trigger"><i className="material-icons">menu</i></a>
+                        <ul className="right hide-on-med-and-down">
+                            <li><Link to="/" className="black-text">Home</Link></li>
+                            <li><Link to="/contact" className="black-text">Contact</Link></li>
+                        </ul>
+                    </div>
+                </nav>
+            </div>
+
+            <ul id="nav-mobile" className="right sidenav">
+                <li><Link to="/" className="black-text">Home</Link></li>
+                <li><Link to="/contact" className="black-text">Contact</Link></li>
+            </ul>
+        </div>
+    );
+}
+
+export default Navbar;
