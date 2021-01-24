b9f508831222d1d49962688f3570e960415abdff
Ryan Rotman <ryan.rotman@gmail.com>
2021-01-23 23:03:13 -0600

    added carousel component

---
+++ src/components/Carousel/index.js
@@ -0,0 +1,25 @@
+import { useEffect } from "react";
+import M from "materialize-css";
+import img1 from "../../assets/images/ccc-1.jpg"
+import img2 from "../../assets/images/ccc-2.jpg"
+import img3 from "../../assets/images/ccc-3.jpg"
+import img4 from "../../assets/images/ccc-4.jpg"
+
+function Carousel() {
+
+    useEffect(() => {
+        let carousel = document.querySelectorAll('.carousel');
+        M.Carousel.init(carousel, {});
+    }, []);
+
+    return (
+        <div class="carousel">
+            <a class="carousel-item" href="#one!"><img src={img1} /></a>
+            <a class="carousel-item" href="#two!"><img src={img2} /></a>
+            <a class="carousel-item" href="#three!"><img src={img3} /></a>
+            <a class="carousel-item" href="#four!"><img src={img4} /></a>
+        </div>
+    );
+}
+
+export default Carousel;
