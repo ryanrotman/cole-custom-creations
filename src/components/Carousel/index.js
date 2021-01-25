5520d40ec2cc5bb6d9ed09f404ae54e3db784764
Ryan Rotman <ryan.rotman@gmail.com>
2021-01-25 00:07:11 -0600

    added indicators

--- src/components/Carousel/index.js
+++ src/components/Carousel/index.js
@@ -4,20 +4,23 @@ import img1 from "../../assets/images/ccc-1.jpg"
 import img2 from "../../assets/images/ccc-2.jpg"
 import img3 from "../../assets/images/ccc-3.jpg"
 import img4 from "../../assets/images/ccc-4.jpg"
+import "./index.css";
 
 function Carousel() {
 
     useEffect(() => {
         let carousel = document.querySelectorAll('.carousel');
-        M.Carousel.init(carousel, {});
+        M.Carousel.init(carousel, { indicators:true });
     }, []);
 
     return (
-        <div class="carousel">
-            <a class="carousel-item" href="#one!"><img src={img1} /></a>
-            <a class="carousel-item" href="#two!"><img src={img2} /></a>
-            <a class="carousel-item" href="#three!"><img src={img3} /></a>
-            <a class="carousel-item" href="#four!"><img src={img4} /></a>
+        <div className="container">
+            <div class="carousel">
+                <a class="carousel-item" href="#one!"><img src={img1} /></a>
+                <a class="carousel-item" href="#two!"><img src={img2} /></a>
+                <a class="carousel-item" href="#three!"><img src={img3} /></a>
+                <a class="carousel-item" href="#four!"><img src={img4} /></a>
+            </div>
         </div>
     );
 }
