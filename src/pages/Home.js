cd57c77c3c97e151d382bb027e113f7c842d8e1b
Ryan Rotman <ryan.rotman@gmail.com>
2021-01-25 00:35:13 -0600

    commented out h1 placeholder

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,13 @@
 import backgroundImg from "../assets/images/grey-wood.jpg";
-import Carousel from "../components/Carousel";
+// import Carousel from "../components/Carousel";
+import Slider from "../components/Slider";
 
 function Home() {
     return (
         <div style={{ backgroundImage: `url(${backgroundImg})`, backgroundRepeat: "no-repeat", height: "100%", backgroundSize: "cover", backgroundAttachment: "fixed", backgroundPosition: "center" }}>
-            <h1 className="center-align">THIS IS THE HOME PAGE</h1>
-            <Carousel />
+            {/* <h1 className="center-align">THIS IS THE HOME PAGE</h1> */}
+            {/* <Carousel /> */}
+            <Slider />
         </div>
     )
 }
