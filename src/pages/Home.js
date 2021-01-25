d6ee9d673d2783ef662954437e9f42645ce1b37c
Ryan Rotman <ryan.rotman@gmail.com>
2021-01-25 14:46:40 -0600

    edited Home page text

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,6 +6,11 @@ function Home() {
     return (
         <div style={{ backgroundImage: `url(${backgroundImg})`, backgroundRepeat: "no-repeat", height: "100%", backgroundSize: "cover", backgroundAttachment: "fixed", backgroundPosition: "center" }}>
             {/* <h1 className="center-align">THIS IS THE HOME PAGE</h1> */}
+            <div className="container center-align">
+                <p><h5>Welcome!</h5></p>
+                <p><h5>We are a husband and wife team specializing in custom corn hole sets.</h5></p>
+            </div>
+            <br />
             {/* <Carousel /> */}
             <Slider />
         </div>
