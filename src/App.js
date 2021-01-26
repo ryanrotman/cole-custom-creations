2cee53d2c7a2c3b73ef936a43556182207e5f669
Ryan Rotman <ryan.rotman@gmail.com>
2021-01-25 23:59:13 -0600

    commented out backgroundImg import

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import backgroundImg from "./assets/images/grey-wood.jpg"
+// import backgroundImg from "./assets/images/grey-wood.jpg"
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
