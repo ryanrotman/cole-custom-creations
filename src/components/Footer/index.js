580e8799283514328e6b81d549c7cc4c6b7bace0
Ryan Rotman <ryan.rotman@gmail.com>
2021-01-26 01:01:13 -0600

    built footer

---
+++ src/components/Footer/index.js
@@ -0,0 +1,13 @@
+function Footer() {
+    return (
+        <footer className="page-footer transparent">
+            <div className="footer-copyright">
+                <div className="container center-align" style={{textShadow: "1px 1px #000000"}}>
+                    Copyright © 2021 Cole Custom Creations
+                </div>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer;
